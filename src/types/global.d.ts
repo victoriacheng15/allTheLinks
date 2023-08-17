dfdb4690bd044484842499321a69af98b6f6aa2c
Victoria Cheng <victoria.cheng15@gmail.com>
2023-08-17 14:14:02 -0600

    chore: add constant data, types, remove most placeholder info

---
+++ src/types/global.d.ts
@@ -0,0 +1,26 @@
+interface List {
+  title: string;
+  href: string;
+  category?: string;
+}
+
+interface Book {
+  title: string;
+  authors: string;
+  imageLinks: {
+    smallThumbnail: string;
+    thumbnail: string;
+  };
+}
+
+interface FetchBooks {
+  books: Book[];
+  loading: boolean;
+  error: string;
+}
+
+interface VolumeInfo {
+  volumeInfo: {
+    title: string;
+  };
+}
