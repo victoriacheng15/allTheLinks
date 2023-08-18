5b5ebcf02ad613df02bfc413c94f2cc79f5a6e2d
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-18 19:45:28 +0000

    chore: add tailwind css and remove existing css

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <HelloWorld message="Vite + Vue" />
+  <h1 class="text-3xl font-bold text-blue-700">Hello from Vue with Tailwind CSS</h1>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
