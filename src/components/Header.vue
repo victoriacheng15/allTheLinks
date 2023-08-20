eee8b7cf52140b6c10e3e1ed3659e4eb9b9a0106
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-20 17:29:37 -0600

    add socials (#9)
    * feat: add formatName function
    
    * feat: add Socials component
    
    * feat: add Socials and change gap to 32px

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { NAME, HEADLINE } from "../constant/"
+import Socials from "./Socials.vue"
 </script>
 
 <template>
   <header class="w-11/12 mx-auto max-w-3xl">
-    <div class="flex flex-col gap-6">
+    <div class="flex flex-col gap-8">
       <img class="h-40 w-40 mx-auto rounded-full" src="/avatar.jpg" alt="" />
       <h1 class="text-center text-3xl font-bold tracking-wide">{{ NAME }}</h1>
       <p class="text-center text-xl font-medium tracking-wide">{{ HEADLINE }}</p>
+      <Socials />
     </div>
   </header>
 </template>
---
+++ src/components/Socials.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { SCOIALS } from "../constant/"
+import { formatName } from "../utils"
+</script>
+
+<template>
+  <ul class="flex justify-center items-center gap-6">
+    <li v-for="social in SCOIALS" :key="social.title"
+      class="p-1 rounded-lg bg-blue-400 duration-300 ease-in-out hover:scale-125">
+      <a :href="social.href" target="_blank" rel="noopener noreferrer">
+        <img :src="`./socials/${formatName(social.title)}.svg`" :alt="social.title" width="48" height="48" />
+      </a>
+    </li>
+  </ul>
+</template>
