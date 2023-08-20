c911065354ffd1cd094377866f6d660541e604c0
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-20 17:00:57 -0600

    add header avatar (#8)
    * feat: add header with avatar, name and headline
    
    * feat: add avatar.jpg
    
    * feat: add NAME and HEADLINE exports
    
    * feat: add header and add padding on top

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { NAME, HEADLINE } from "../constant/"
+</script>
+
+<template>
+  <header class="w-11/12 mx-auto max-w-3xl">
+    <div class="flex flex-col gap-6">
+      <img class="h-40 w-40 mx-auto rounded-full" src="/avatar.jpg" alt="" />
+      <h1 class="text-center text-3xl font-bold tracking-wide">{{ NAME }}</h1>
+      <p class="text-center text-xl font-medium tracking-wide">{{ HEADLINE }}</p>
+    </div>
+  </header>
+</template>
