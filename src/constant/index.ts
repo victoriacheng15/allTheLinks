c80b7722930b1efcdd113cf7629745d20d5e855b
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-21 15:17:30 -0600

    add prettier rome (#15)
    * feat: add rome and prettier packages
    
    * feat: add rome.json
    
    * chore: add files for prettier to ignore
    
    * fix: correct files type, ts instead of tsx

---
+++ src/constant/index.ts
@@ -0,0 +1,6 @@
+export { SCOIALS, LINKS } from "./links";
+export { BOOKS, PLACEHOLDERS } from "./books";
+
+export const NAME = "Victoria Cheng";
+export const HEADLINE =
+	"🌊 🖥 Software Developer | React | TypeScript | Next.js | Redux | Node.js | MongoDB | ✍ Blogger";
