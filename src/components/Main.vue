83ff9e39e29331c2c1d408656b1fcfa40f560a9f
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-21 10:06:53 -0600

    add main content (#12)
    * fix: add alt for the avatar
    
    * chore: change function name to formatNameForSVG to be more clear
    
    * chore: change hover style for social icons and utilize destructure for loop
    
    * feat: create Main content placeholder
    
    * feat: add Main component to App

---
+++ src/components/Main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { LINKS } from '../constant';
+</script>
+
+<template>
+  <main class="w-11/12 mx-auto max-w-3xl py-8">
+    <section>
+      <p>test</p>
+    </section>
+  </main>
+</template>
