40adf9bd7328a13679e0782c353bcf752417561d
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-21 10:39:06 -0600

    feat: add links to main content (#13)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,9 +3,18 @@ import { LINKS } from '../constant';
 </script>
 
 <template>
-  <main class="w-11/12 mx-auto max-w-3xl py-8">
+  <main class="w-11/12 mx-auto max-w-3xl py-14">
     <section>
-      <p>test</p>
+      <ul class="flex flex-col gap-8 text-center">
+        <li v-for="{ title, href } in LINKS" :key="title" class="overflow-hidden rounded-lg">
+          <a :href="href" target="_blank" rel="noopener noreferrer" class="text-2xl">
+            <div
+              class="group w-full h-full p-3 bg-blue-800 relative before:absolute before:inset-0 before:w-0 before:hover:w-full before:duration-300 before:ease-in-out before:bg-blue-300">
+              <span class="relative z-20 font-medium tracking-wider group-hover:text-gray-900">{{ title }}</span>
+            </div>
+          </a>
+        </li>
+      </ul>
     </section>
   </main>
 </template>
