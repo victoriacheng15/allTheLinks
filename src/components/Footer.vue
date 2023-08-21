2653134dca6a6e0616ec762762a0f05a60fb388e
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-21 11:06:22 -0600

    add footer (#14)
    * feat: create footer component
    
    * feat: add footer component to App
    
    * chore: add style for #app
    
    * chore: add padding top for header

---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { NAME } from '../constant';
+</script>
+
+<template>
+  <footer class="mt-auto py-4">
+    <p class="text-center">Copyright &copy; <span aria-label="penguin emoji">🐧</span> {{ NAME }}</p>
+  </footer>
+</template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@ import Socials from "./Socials.vue"
 </script>
 
 <template>
-  <header class="w-11/12 mx-auto max-w-3xl">
+  <header class="w-11/12 mx-auto max-w-3xl pt-20">
     <div class="flex flex-col gap-8">
       <img class="h-40 w-40 mx-auto rounded-full" src="/avatar.jpg" alt="Victoria Cheng" />
       <h1 class="text-center text-3xl font-bold tracking-wide">{{ NAME }}</h1>
