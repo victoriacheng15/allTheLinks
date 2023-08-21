5cce82fa04e952860b336176655d68b24660ae4d
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-21 16:00:19 +0000

    chore: change hover style for social icons and utilize destructure for loop

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { SCOIALS } from "../constant/"
-import { formatName } from "../utils"
+import { formatNameForSVG } from "../utils"
 </script>
 
 <template>
   <ul class="flex justify-center items-center gap-6">
-    <li v-for="social in SCOIALS" :key="social.title"
-      class="p-1 rounded-lg bg-blue-400 duration-300 ease-in-out hover:scale-125">
-      <a :href="social.href" target="_blank" rel="noopener noreferrer">
-        <img :src="`./socials/${formatName(social.title)}.svg`" :alt="social.title" width="48" height="48" />
+    <li v-for="{ title, href } in SCOIALS" :key="title"
+      class="p-1 rounded-lg overflow-hidden duration-300 ease-in-out relative before:absolute before:inset-0 before:-z-10 before:bg-gradient-to-t from-blue-400 to-blue-600 before:w-full before:hover:from-blue-600 before:hover:to-blue-400 before:duration-300 before:ease-in-out">
+      <a :href="href" target="_blank" rel="noopener noreferrer">
+        <img :src="`./socials/${formatNameForSVG(title)}.svg`" :alt="title" width="48" height="48" />
       </a>
     </li>
   </ul>
