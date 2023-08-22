aceda7c8ed780096cde13307c5db53c95638e2b1
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-22 15:41:26 +0000

    style: the files have been formatted with tailwindcss plugin

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,10 +5,10 @@ defineProps<{ name: string; headline: string }>();
 </script>
 
 <template>
-  <header class="w-11/12 mx-auto max-w-3xl pt-20">
+  <header class="mx-auto w-11/12 max-w-3xl pt-20">
     <div class="flex flex-col gap-8">
       <img
-        class="h-40 w-40 mx-auto rounded-full"
+        class="mx-auto h-40 w-40 rounded-full"
         src="/avatar.jpg"
         alt="{{ name }}"
       />
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,7 @@ import { LINKS } from "../constant";
 </script>
 
 <template>
-  <main class="w-11/12 mx-auto max-w-3xl py-14">
+  <main class="mx-auto w-11/12 max-w-3xl py-14">
     <section>
       <ul class="flex flex-col gap-8 text-center">
         <li
@@ -18,7 +18,7 @@ import { LINKS } from "../constant";
             class="text-2xl"
           >
             <div
-              class="group w-full h-full p-3 bg-blue-800 relative before:absolute before:inset-0 before:w-0 before:hover:w-full before:duration-300 before:ease-in-out before:bg-blue-300"
+              class="group relative h-full w-full bg-blue-800 p-3 before:absolute before:inset-0 before:w-0 before:bg-blue-300 before:duration-300 before:ease-in-out before:hover:w-full"
             >
               <span
                 class="relative z-20 font-medium tracking-wider group-hover:text-gray-900"
