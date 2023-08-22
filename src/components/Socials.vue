e7ea8be89751794963b9da228f168b8ee2e82d39
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-22 09:43:57 -0600

    add prettier tw (#19)
    * feat: add prettier plugin for tailwind css
    
    * chore: add prettier config
    
    * style: the files have been formatted with tailwindcss plugin

--- src/components/Socials.vue
+++ src/components/Socials.vue
@@ -4,11 +4,11 @@ import { formatNameForSVG } from "../utils";
 </script>
 
 <template>
-  <ul class="flex justify-center items-center gap-6">
+  <ul class="flex items-center justify-center gap-6">
     <li
       v-for="{ title, href } in SCOIALS"
       :key="title"
-      class="p-1 rounded-lg overflow-hidden duration-300 ease-in-out relative before:absolute before:inset-0 before:-z-10 before:bg-gradient-to-t from-blue-400 to-blue-600 before:w-full before:hover:from-blue-600 before:hover:to-blue-400 before:duration-300 before:ease-in-out"
+      class="relative overflow-hidden rounded-lg from-blue-400 to-blue-600 p-1 duration-300 ease-in-out before:absolute before:inset-0 before:-z-10 before:w-full before:bg-gradient-to-t before:duration-300 before:ease-in-out before:hover:from-blue-600 before:hover:to-blue-400"
     >
       <a :href="href" target="_blank" rel="noopener noreferrer">
         <img
