e619b03ef71c69fe80780128b5fe535f3ad76be1
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-22 09:16:03 -0600

    chore: remove files due to incorrect ffile format (#17)

--- src/constant/index.tsx
+++
@@ -1,6 +0,0 @@
-export { SCOIALS, LINKS } from "./links";
-export { BOOKS, PLACEHOLDERS } from "./books";
-
-export const NAME = "Victoria Cheng";
-export const HEADLINE =
-  "🌊 🖥 Software Developer | React | TypeScript | Next.js | Redux | Node.js | MongoDB | ✍ Blogger";
