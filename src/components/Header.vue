3e8e31e95e6fb0f809332136bdbd60a37c3f57d3
victoriacheng15 <victoriacheng15@users.noreply.github.com>
2023-08-22 15:27:52 +0000

    style: the files have been formatted

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import Socials from "./Socials.vue";
 
-defineProps<{ name: string, headline: string }>();
+defineProps<{ name: string; headline: string }>();
 </script>
 
 <template>
   <header class="w-11/12 mx-auto max-w-3xl pt-20">
     <div class="flex flex-col gap-8">
-      <img class="h-40 w-40 mx-auto rounded-full" src="/avatar.jpg" alt="{{ name }}" />
+      <img
+        class="h-40 w-40 mx-auto rounded-full"
+        src="/avatar.jpg"
+        alt="{{ name }}"
+      />
       <h1 class="text-center text-3xl font-bold tracking-wide">{{ name }}</h1>
       <p class="text-center text-xl tracking-wide">{{ headline }}</p>
       <Socials />
