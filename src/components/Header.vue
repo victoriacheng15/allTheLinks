db0d7ccc0b46893346acfab6e0e1c9a905477303
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-22 15:25:57 +0000

    chore: add defineProps for name and headline

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
-import { NAME, HEADLINE } from "../constant/";
 import Socials from "./Socials.vue";
+
+defineProps<{ name: string, headline: string }>();
 </script>
 
 <template>
   <header class="w-11/12 mx-auto max-w-3xl pt-20">
     <div class="flex flex-col gap-8">
-      <img
-        class="h-40 w-40 mx-auto rounded-full"
-        src="/avatar.jpg"
-        alt="Victoria Cheng"
-      />
-      <h1 class="text-center text-3xl font-bold tracking-wide">{{ NAME }}</h1>
-      <p class="text-center text-xl tracking-wide">{{ HEADLINE }}</p>
+      <img class="h-40 w-40 mx-auto rounded-full" src="/avatar.jpg" alt="{{ name }}" />
+      <h1 class="text-center text-3xl font-bold tracking-wide">{{ name }}</h1>
+      <p class="text-center text-xl tracking-wide">{{ headline }}</p>
       <Socials />
     </div>
   </header>
