99e67b8f9e1782b312f7b6cb0b99e2d802d33844
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-23 20:40:24 +0000

    feat: add formatAuthors function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,15 @@
-export const formatNameForSVG = (title: string) =>
-	title.toLowerCase().split(" ").join("");
+export function formatNameForSVG(title: string) {
+	return title.toLowerCase().split(" ").join("");
+}
+
+export function formatAuthors(authors: string[]): string {
+	const authorCount = authors.length
+	const formatOptions = {
+		1: `${authors}`,
+		2: `${authors.join(" and ")}`,
+	}
+
+	const threeAuthorsPlus = `${authors.slice(0, -1).join(", ")}, and ${authors.slice(-1)}`
+
+	return formatOptions[authorCount as keyof typeof formatOptions] || threeAuthorsPlus
+}
