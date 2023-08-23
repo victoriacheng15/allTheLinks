b15cbc932b1163ee5c491cd63dfb3487cf0a63fa
victoriacheng15 <victoriacheng15@users.noreply.github.com>
2023-08-23 20:40:52 +0000

    style: the files have been formatted

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
 import { BOOKS } from "../constant";
-import { formatAuthors } from "../utils"
+import { formatAuthors } from "../utils";
 
-const books = ref<Book[]>([])
-const loading = ref(false)
-const errorMsg = ref<string>("")
+const books = ref<Book[]>([]);
+const loading = ref(false);
+const errorMsg = ref<string>("");
 
 async function getBooksInfo(bookTitle: string, author: string) {
-  const PARMS = '&printType=books&projection=lite';
+  const PARMS = "&printType=books&projection=lite";
   const URL = `https://www.googleapis.com/books/v1/volumes?q=+intitle:${bookTitle}+inauthor:${author}
   ${PARMS}`;
 
@@ -18,7 +18,7 @@ async function getBooksInfo(bookTitle: string, author: string) {
     const myBooks = data.items.find(
       ({ volumeInfo: { title } }: VolumeInfo) => title === bookTitle
     );
-    console.log(myBooks)
+    console.log(myBooks);
     return myBooks.volumeInfo;
   } catch (err) {
     throw err;
@@ -35,23 +35,27 @@ onMounted(async () => {
     books.value = bookInfos;
   } catch (err) {
     errorMsg.value = err as string;
-    console.error('Failed to fetch book information:', errorMsg.value);
+    console.error("Failed to fetch book information:", errorMsg.value);
   } finally {
     loading.value = false;
   }
 });
-
 </script>
 
 <template>
-  <h2 class="py-6 text-center text-3xl">
-    📚 My Book Recommendations: 📚
-  </h2>
+  <h2 class="py-6 text-center text-3xl">📚 My Book Recommendations: 📚</h2>
   <p v-if="loading">it is loading at this moment</p>
   <ul class="flex flex-wrap items-center justify-center gap-6">
-    <li v-for="{ title, authors, imageLinks: { thumbnail } } in books" :key="title"
-      class="grid place-items-center gap-1 w-56">
-      <img class="h-full w-full" :src="thumbnail" :alt="`${title} Book by ${formatAuthors(authors)}`">
+    <li
+      v-for="{ title, authors, imageLinks: { thumbnail } } in books"
+      :key="title"
+      class="grid w-56 place-items-center gap-1"
+    >
+      <img
+        class="h-full w-full"
+        :src="thumbnail"
+        :alt="`${title} Book by ${formatAuthors(authors)}`"
+      />
       <h3 class="text-center text-xl">
         {{ title }}
       </h3>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,13 +3,17 @@ export function formatNameForSVG(title: string) {
 }
 
 export function formatAuthors(authors: string[]): string {
-	const authorCount = authors.length
+	const authorCount = authors.length;
 	const formatOptions = {
 		1: `${authors}`,
 		2: `${authors.join(" and ")}`,
-	}
+	};
 
-	const threeAuthorsPlus = `${authors.slice(0, -1).join(", ")}, and ${authors.slice(-1)}`
+	const threeAuthorsPlus = `${authors
+		.slice(0, -1)
+		.join(", ")}, and ${authors.slice(-1)}`;
 
-	return formatOptions[authorCount as keyof typeof formatOptions] || threeAuthorsPlus
+	return (
+		formatOptions[authorCount as keyof typeof formatOptions] || threeAuthorsPlus
+	);
 }
