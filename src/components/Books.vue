40e86da6bb099a5c6c7f05f8220283f8122b3a6d
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-25 12:32:13 +0000

    fix: move getBooksInfo function back to Books.vue

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-import { getBooksInfo } from "../composables/useFetchBooks.vue";
 import Placeholders from "./DisplayPlaceholders.vue";
 import { BOOKS } from "../constant";
 import { formatAuthors } from "../utils";
@@ -8,6 +7,24 @@ import { formatAuthors } from "../utils";
 const books = ref<Book[]>([]);
 const loading = ref(false);
 const errorMsg = ref<string>("");
+
+async function getBooksInfo(bookTitle: string, author: string) {
+  const PARMS = "&printType=books&projection=lite";
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=+intitle:${bookTitle}+inauthor:${author}
+  ${PARMS}`;
+
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const myBooks = data.items.find(
+      ({ volumeInfo: { title } }: VolumeInfo) => title === bookTitle
+    );
+    console.log(myBooks);
+    return myBooks.volumeInfo;
+  } catch (err) {
+    throw err;
+  }
+}
 
 onMounted(async () => {
   loading.value = true;
@@ -30,16 +47,9 @@ onMounted(async () => {
   <h2 class="py-6 text-center text-3xl">📚 My Book Recommendations: 📚</h2>
   <Placeholders :isLoading="loading" />
   <ul class="flex flex-wrap items-center justify-center gap-6">
-    <li
-      v-for="{ title, authors, imageLinks: { thumbnail } } in books"
-      :key="title"
-      class="grid w-56 place-items-center gap-1"
-    >
-      <img
-        class="h-full w-full"
-        :src="thumbnail"
-        :alt="`${title} Book by ${formatAuthors(authors)}`"
-      />
+    <li v-for="{ title, authors, imageLinks: { thumbnail } } in books" :key="title"
+      class="grid w-56 place-items-center gap-1">
+      <img class="h-full w-full" :src="thumbnail" :alt="`${title} Book by ${formatAuthors(authors)}`" />
       <h3 class="text-center text-xl">
         {{ title }}
       </h3>
