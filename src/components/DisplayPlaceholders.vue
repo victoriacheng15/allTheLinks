7351fe93187461b984a9cbb89d7a0a8bc523f596
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-25 12:23:48 +0000

    feat: create displayPlaceholders components

---
+++ src/components/DisplayPlaceholders.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { PLACEHOLDERS } from "../constant";
+
+defineProps<{ isLoading: boolean }>();
+</script>
+
+<template>
+  <ul v-if="isLoading" class="flex flex-wrap items-center justify-center gap-6">
+    <li v-for=" placeholder in PLACEHOLDERS" :key="placeholder" class="w-56">
+      <div class="h-72 w-full bg-gray-50 opacity-25 blur-sm" />
+      <div class="h-7 w-full bg-gray-50 opacity-30 blur-sm" />
+    </li>
+  </ul>
+</template>
