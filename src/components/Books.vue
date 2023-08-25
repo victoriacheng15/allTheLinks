82661a4d88301f6acb900521a7ceb1f27416983f
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-08-25 06:32:55 -0600

    fix: move getBooksInfo function back to Books.vue (#24)
    * fix: move getBooksInfo function back to Books.vue
    
    * style: the files have been formatted
    
    ---------
    
    Co-authored-by: victoriacheng15 <victoriacheng15@users.noreply.github.com>

--- src/components/Books.vue
+++ src/components/Books.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-import { getBooksInfo } from "../composables/useFetchBooks.vue";
 import Placeholders from "./DisplayPlaceholders.vue";
 import { BOOKS } from "../constant";
 import { formatAuthors } from "../utils";
@@ -8,6 +7,24 @@ import { formatAuthors } from "../utils";
 const books = ref<Book[]>([]);
 const loading = ref(false);
 const errorMsg = ref<string>("");
+
+async function getBooksInfo(bookTitle: string, author: string) {
+  const PARMS = "&printType=books&projection=lite";
+  const URL = `https://www.googleapis.com/books/v1/volumes?q=+intitle:${bookTitle}+inauthor:${author}
+  ${PARMS}`;
+
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const myBooks = data.items.find(
+      ({ volumeInfo: { title } }: VolumeInfo) => title === bookTitle
+    );
+    console.log(myBooks);
+    return myBooks.volumeInfo;
+  } catch (err) {
+    throw err;
+  }
+}
 
 onMounted(async () => {
   loading.value = true;
