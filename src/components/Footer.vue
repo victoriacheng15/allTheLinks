04edd175f5f5340f48a89ac733486779525d394d
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2024-01-03 09:24:47 -0700

    chore: add link to the repo (#46)
    * chore: add link to the repo
    
    * style: the files have been formatted
    
    ---------
    
    Co-authored-by: victoriacheng15 <victoriacheng15@users.noreply.github.com>

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,9 @@ defineProps<{ name: string }>();
 <template>
   <footer class="mt-auto py-4">
     <p class="text-center">
-      Copyright &copy; <span aria-label="penguin emoji">🐧</span> {{ name }}
+      Copyright &copy; <span aria-label="penguin emoji">🐧</span> {{ name }} | <span><a
+          href="https://github.com/victoriacheng15/allTheLinks" target="_blank" rel="noopener noreferrer">👋<img
+            class="h-4 w-4 inline ml-2" src="./socials/github.svg" alt="github" /></a></span>
     </p>
   </footer>
 </template>
